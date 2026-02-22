e0f45da281c135c2730b7629435d75f510a1792e
FelisbinoJose <246760.jose@unibave.net>
2026-02-22 15:06:26 -0300

    FEAT: adiciona mensagem com concatenação

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 alert("Bem vindo ao jogo do número segreto");
 
-let numeroSecreto = 29;
+let numeroSecreto = 5;
 
 let chute = prompt("Informe um número entre 1 e 10");
 
 if (chute == numeroSecreto) {
-    console.log("Parabéns");
-    
+    alert(`Parabéns!! Você acertou o número secreto (${numeroSecreto})`);
+} else {
+    alert("Você errou!");
 }
