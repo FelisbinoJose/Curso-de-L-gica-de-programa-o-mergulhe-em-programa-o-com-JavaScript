cfa7d2053eafd076a901a78a1b7d3b73e3bd2fb6
FelisbinoJose <246760.jose@unibave.net>
2026-02-22 15:18:09 -0300

    FEAT: adiciona verificações no console

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-alert("Bem vindo ao jogo do número segreto");
+alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+console.log('Valor do chute:', chute);
 
-let chute = prompt("Informe um número entre 1 e 10");
+let numeroSecreto = 4;
+
+console.log('Resultado da comparação:', chute == numeroSecreto);
 
 if (chute == numeroSecreto) {
-    alert(`Parabéns!! Você acertou o número secreto (${numeroSecreto})`);
+    alert('Acertou');
 } else {
-    alert("Você errou!");
+    console.log('Valor do número secreto:', numeroSecreto);
+    alert('O número secreto era ' + numeroSecreto);
 }
