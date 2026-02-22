57922dabae997d08cf5cbb885a4ceeab8d5775f6
FelisbinoJose <246760.jose@unibave.net>
2026-02-22 16:44:02 -0300

    FEAT: adiciona quebra de código

--- app.js
+++ app.js
@@ -12,16 +12,20 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
 
         if (chute == numeroSecreto) {
-        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
-        
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-
         tentativas++;
     }
+}
+
+if (tentativas > 1) {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
 }
 
