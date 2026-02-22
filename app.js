ceb5fab6feb36a8f15722edb04935cb70a8ac577
FelisbinoJose <246760.jose@unibave.net>
2026-02-22 14:08:41 -0300

    FEAT: adiciona comparação de variaveis

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 alert("Bem vindo ao jogo do número segreto");
 
-const numeroSecreto = 29;
+let numeroSecreto = 29;
 
-prompt("Informe um número entre 1 e 10");
+let chute = prompt("Informe um número entre 1 e 10");
+
+if (chute == numeroSecreto) {
+    console.log("Parabéns");
+    
+}
